0c095cda81cd89b8d6cd3cb900cd0d22ff0731f7
Dan Safee <dan@getdrx.com>
2024-10-14 07:38:44 -0400

    Initial Commit

---
+++ venv/lib/python3.10/site-packages/pysword/modules.py
@@ -0,0 +1,220 @@
+# -*- coding: utf-8 -*-
+###############################################################################
+# PySword - A native Python reader of the SWORD Project Bible Modules         #
+# --------------------------------------------------------------------------- #
+# Copyright (c) 2008-2019 Various PySword developers:                         #
+# Kenneth Arnold, Joshua Gross, Tomas Groth, Ryan Hiebert, Philip Ridout,     #
+# Matthew Wardrop                                                             #
+# --------------------------------------------------------------------------- #
+# Permission is hereby granted, free of charge, to any person obtaining a     #
+# copy of this software and associated documentation files (the "Software"),  #
+# to deal in the Software without restriction, including without limitation   #
+# the rights to use, copy, modify, merge, publish, distribute, sublicense,    #
+# and/or sell copies of the Software, and to permit persons to whom the       #
+# Software is furnished to do so, subject to the following conditions:        #
+#                                                                             #
+# The above copyright notice and this permission notice shall be included     #
+# in all copies or substantial portions of the Software.                      #
+#                                                                             #
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR  #
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,    #
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE #
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER      #
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING     #
+# FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER         #
+# DEALINGS IN THE SOFTWARE.                                                   #
+###############################################################################
+
+import io
+import os
+import zipfile
+import tempfile
+import shutil
+import sys
+
+from pysword.bible import SwordBible, BlockType, CompressType
+from pysword.utils import path_like_to_str
+
+
+def get_platform_path():
+    """
+    Returns the default SWORD datapath for the current platform.
+
+    This is useful if you want to search for modules in both the default path
+    and some additional paths.
+    """
+
+    if sys.platform.startswith(u'win32'):
+        return os.path.join(os.getenv(u'APPDATA'), u'Sword')
+    elif sys.platform.startswith(u'darwin'):
+        return os.path.join(os.getenv(u'HOME'), u'Library', u'Application Support', u'Sword')
+    else:  # Linux etc.
+        return os.path.join(os.getenv(u'HOME'), u'.sword')
+
+
+class SwordModules(object):
+    """
+    Class used to parse module conf-files.
+    """
+    def __init__(self, paths=None, encoding=None):
+        """
+        Initialize the SwordModules object.
+
+        :param paths: Path the SWORD datapath or to a zip-file containing a
+                      module. Defaults to the platform's expected SWORD
+                      datapath. A list of paths may be given. In that case,
+                      modules will be loaded from all of the paths, with later
+                      paths taking precedence.
+        :param encoding: The encoding to use when reading the text.
+        """
+        self._encoding = encoding
+
+        if paths is None:
+            # Based on the platform find the SWORD data path
+            self._sword_paths = [get_platform_path()]
+        else:
+            if not isinstance(paths, list):
+                paths = [paths]
+
+            try:
+                self._sword_paths = [path_like_to_str(path) for path in paths]
+            except TypeError:
+                raise TypeError(u'`path` should be a str, PathLike object or an instance of pathlib.Path')
+
+        self._modules = {}
+        self._module_paths = {}
+        self._temp_folders = []
+
+    def __del__(self):
+        """
+        Clean up. If we decompressed a zip-file remove the files again.
+        """
+        for temp_folder in self._temp_folders:
+            # TODO: This sometimes fails because it is getting GC'ed before the files in the bible module are closed!
+            shutil.rmtree(temp_folder, ignore_errors=True)
+
+    def _parse_conf_file(self, conf_filename, module_folder):
+        """
+        Parse a module conf file and add the result to module dict
+
+        :param conf_filename: The conf file to parse.
+        :param module_folder: The folder the module is in
+        """
+        # Open configuration file
+        try:
+            conf_file_lines = []
+            if self._encoding is None:
+                # No encoding specified try uft-8
+                try:
+                    with io.open(conf_filename, mode=u'rt', encoding=u'utf-8', errors=u'strict') as conf_file:
+                        conf_file_lines = conf_file.readlines()
+                except UnicodeDecodeError:
+                    # Fallback to iso=8859-1 (latin-1)
+                    self._encoding = u'iso-8859-1'
+            if self._encoding:
+                with io.open(conf_filename, mode=u'rt', encoding=self._encoding, errors=u'strict') as conf_file:
+                    conf_file_lines = conf_file.readlines()
+        except Exception as e:
+            print(u'Exception %s caught while parsing %s\n%s' % (sys.exc_info()[0].__name__, f, e))
+            return
+        # Start parsing configuration file
+        module_conf = dict()
+        module_name = u''
+        cur_key = u''
+        for line in conf_file_lines:
+            # Ignore comments
+            if line.startswith(u'#'):
+                continue
+            line = line.strip()
+            # Detect start of module configurations. Support multiple module configurations in one file.
+            if line.startswith(u'[') and line.endswith(u']'):
+                # If a configuration was already parsed, save it.
+                if module_name:
+                    self._modules[module_name] = module_conf
+                    self._module_paths[module_name] = module_folder
+                # Set the module name, strip the '[' and ']'
+                module_name = line[1:-1]
+                module_conf = dict()
+                continue
+            # Handle configuration line. Try to split the line by '='.
+            line_split = line.split(u'=', 1)
+            # if a '=' was detected a new configuration key was found
+            if len(line_split) > 1:
+                cur_key = line_split[0].lower()
+                # Join the remaining elements using a '=', this makes it possible to have a '=' in the value part
+                module_conf[cur_key] = u'='.join(line_split[1:])
+            else:
+                if cur_key and line:
+                    module_conf[cur_key] += u'\n' + line
+        if module_name:
+            self._modules[module_name] = module_conf
+            self._module_paths[module_name] = module_folder
+
+    def parse_modules(self):
+        """
+        Based on the datapath given to the constructor parse modules conf-files and return the result
+
+        :return: A dict containing the data read from the conf files.
+        """
+        for path in self._sword_paths:
+            # If path is a zipfile, we extract it to a temp-folder
+            if path.endswith(u'.zip'):
+                module_folder = tempfile.mkdtemp()
+                self._temp_folders.append(module_folder)
+                zipped_module = zipfile.ZipFile(path)
+                zipped_module.extractall(module_folder)
+            else:
+                module_folder = path
+
+            conf_folder = os.path.join(module_folder, u'mods.d')
+
+            # Loop over config files and save data in a dict
+            for f in os.listdir(conf_folder):
+                if f.endswith(u'.conf'):
+                    conf_filename = os.path.join(conf_folder, f)
+                    self._parse_conf_file(conf_filename, module_folder)
+
+        # Return the modules metadata
+        return self._modules
+
+    def get_bible_from_module(self, module_key, cipher=None):
+        """
+        Return a SwordBible object for the key given.
+
+        :param module_key: The key to use for finding the module.
+        :param cipher: Cipher key to use to decrypt module. Overrules any cipher in the module conf
+        :return: a SwordBible object for the key given.
+        """
+        # TODO could replace the try excepts with a simple.get
+        bible_module = self._modules[module_key]
+        module_path = os.path.join(self._module_paths[module_key], bible_module[u'datapath'])
+        module_type = bible_module[u'moddrv'].lower()
+        try:
+            module_versification = bible_module[u'versification'].lower()
+        except KeyError:
+            module_versification = u'kjv'
+        try:
+            module_encoding = bible_module[u'encoding'].lower()
+        except KeyError:
+            module_encoding = None
+        try:
+            source_type = bible_module[u'sourcetype']
+        except KeyError:
+            source_type = None
+        try:
+            block_type = bible_module[u'blocktype']
+        except KeyError:
+            block_type = BlockType.BOOK
+        try:
+            compress_type = bible_module[u'compress_type']
+        except KeyError:
+            compress_type = CompressType.ZIP
+        if cipher:
+            cipherkey = cipher
+        else:
+            try:
+                cipherkey = bible_module[u'cipherkey']
+            except KeyError:
+                cipherkey = None
+        return SwordBible(module_path, module_type, module_versification, module_encoding, source_type, block_type,
+                          compress_type, cipherkey)
---
+++ venv/lib/python3.10/site-packages/pysword/sapphire.py
@@ -0,0 +1,257 @@
+# -*- coding: utf-8 -*-
+"""
+    This is a direct port of the original C++ code to Python done by Tomas Groth in 2019.
+    The original copyright note is below. This port is also Public Domain.
+
+/* sapphire.cpp -- the Sapphire II stream cipher class.
+   Dedicated to the Public Domain the author and inventor:
+   (Michael Paul Johnson).  This code comes with no warranty.
+   Use it at your own risk.
+   Ported from the Pascal implementation of the Sapphire Stream
+   Cipher 9 December 1994.
+   Added hash pre- and post-processing 27 December 1994.
+   Modified initialization to make index variables key dependent,
+   made the output function more resistant to cryptanalysis,
+   and renamed to Sapphire II 2 January 1995
+*/
+"""
+
+
+class Sapphire:
+
+    def __init__(self, key):
+        """
+        Constructor. Initializes the stream cipher.
+
+        :param key: The cipher key to use.
+        """
+        self.initial_cards = [0] * 256
+        self.initial_rsum = 0
+        self.cards = [0] * 256
+        self.rotor = 0
+        self.ratchet = 0
+        self.avalanche = 0
+        self.last_plain = 0
+        self.last_cipher = 0
+        bytes_key = bytearray(key, 'utf-8')
+        if key:
+            self._initialize(bytes_key, len(bytes_key))
+
+    def _initialize(self, key, keysize):
+        """
+        Key size may be up to 256 bytes.
+        Pass phrases may be used directly, with longer length
+        compensating for the low entropy expected in such keys.
+        Alternatively, shorter keys hashed from a pass phrase or
+        generated randomly may be used. For random keys, lengths
+        of from 4 to 16 bytes are recommended, depending on how
+        secure you want this to be.
+
+        :param key: The cipher key to use.
+        :param keysize: The size of the cipher key.
+        """
+
+        # If we have been given no key, assume the default hash setup.
+        if not key:
+            self._hash_init()
+            return
+
+        # Start with cards all in order, one of each.
+        self.cards = list(range(0, 256))
+
+        # Swap the card at each position with some other card.
+        toswap = 0
+        keypos = 0  # Start with first byte of user key.
+        rsum = 0
+        for i in range(255, -1, -1):
+            toswap, rsum, keypos = self._keyrand(i, key, keysize, rsum, keypos)
+            swaptemp = self.cards[i]
+            self.cards[i] = self.cards[toswap]
+            self.cards[toswap] = swaptemp
+
+        # Initialize the indices and data dependencies.
+        # Indices are set to different values instead of all 0
+        # to reduce what is known about the state of the cards
+        # when the first byte is emitted.
+        self.rotor = self.cards[1]
+        self.ratchet = self.cards[3]
+        self.avalanche = self.cards[5]
+        self.last_plain = self.cards[7]
+        self.last_cipher = self.cards[rsum]
+
+        # Save initial cards and rsum for easy reset, might be a security risk.
+        # Note that this was added for python and was not in the original C++ code
+        self.initial_cards = self.cards[:]
+        self.initial_rsum = rsum
+
+    def reset(self):
+        """
+        Resets the cipher stream for new data.
+        Note that this was added for python and was not in the original C++ code.
+        The data save to be able to reset might be a security risk.
+        """
+        if self.initial_cards:
+            self.cards = self.initial_cards[:]
+            self.rotor = self.cards[1]
+            self.ratchet = self.cards[3]
+            self.avalanche = self.cards[5]
+            self.last_plain = self.cards[7]
+            self.last_cipher = self.cards[self.initial_rsum]
+        else:
+            self._hash_init()
+
+    def _keyrand(self, limit, user_key, keysize, rsum, keypos):
+        """
+        Helper function used to determine which cards to swap randomly
+        based on the user key.
+
+        :param limit: Value from 0 to limit to return.
+        :param user_key: The user key.
+        :param keysize: Size of the user key.
+        :param keypos: Current position/index of the user key to start from.
+        :return: Three values are returned: card to swap, new rsum, new keypos
+        """
+        u = limit + 1      # Value from 0 to limit to return.
+        retry_limiter = 0  # No infinite loops allowed.
+        mask = 0           # Select just enough bits.
+
+        if limit == 0:  # Avoid divide by zero error.
+            return 0, rsum, keypos
+        mask = 1                # Fill mask with enough bits to cover
+        while mask < limit:     # the desired range.
+            mask = (mask << 1) + 1
+
+        while (u > limit):
+            rsum = self.cards[rsum] + user_key[keypos]
+            keypos += 1
+            if keypos >= keysize:
+                keypos = 0        # Recycle the user key.
+                rsum += keysize   # key "aaaa" != key "aaaaaaaa"
+            rsum &= 0xFF
+            u = mask & rsum
+            retry_limiter += 1
+            if retry_limiter > 11:
+                u %= limit     # Prevent very rare long loops.
+
+        return u, rsum, keypos
+
+    def _hash_init(self):
+        """
+        This function is used to initialize non-keyed hash
+        computation.
+        """
+        # Initialize the indices and data dependencies.
+        self.rotor = 1
+        self.ratchet = 3
+        self.avalanche = 5
+        self.last_plain = 7
+        self.last_cipher = 11
+
+        # Start with cards all in inverse order.
+        self.cards = list(range(255, -1, -1))
+
+    def _burn(self):
+        """
+        Destroy the key and state information in RAM.
+        """
+        self.initial_cards = [0] * 256
+        self.initial_rsum = 0
+        self.cards = [0] * 256
+        self.rotor = 0
+        self.ratchet = 0
+        self.avalanche = 0
+        self.last_plain = 0
+        self.last_cipher = 0
+
+    def __del__(self):
+        """
+        Destructor
+        """
+        self._burn()
+
+    def encrypt(self, b):
+        """
+        Picture a single enigma rotor with 256 positions, rewired
+        on the fly by card-shuffling.
+        This cipher is a variant of one invented and written
+        by Michael Paul Johnson in November, 1993.
+
+        :param b: Byte to encrypt
+        :return: Encrypted byte
+        """
+        swaptemp = 0
+
+        # Shuffle the deck a little more.
+        self.ratchet = (self.ratchet + self.cards[self.rotor]) & 0xFF
+        self.rotor = (1 + self.rotor) & 0xFF
+        swaptemp = self.cards[self.last_cipher]
+        self.cards[self.last_cipher] = self.cards[self.ratchet]
+        self.cards[self.ratchet] = self.cards[self.last_plain]
+        self.cards[self.last_plain] = self.cards[self.rotor]
+        self.cards[self.rotor] = swaptemp
+        self.avalanche = (self.avalanche + self.cards[swaptemp]) & 0xFF
+
+        # Output one byte from the state in such a way as to make it
+        # very hard to figure out which one you are looking at.
+        self.last_cipher = b ^ self.cards[(self.cards[self.ratchet] + self.cards[self.rotor]) & 0xFF] ^ \
+            self.cards[self.cards[(self.cards[self.last_plain] + self.cards[self.last_cipher] +
+                                   self.cards[self.avalanche]) & 0xFF]]
+        self.last_plain = b
+        return self.last_cipher
+
+    def decrypt_bytes(self, encrypted_data):
+        """
+        Helper function to make it easier for the users to decrypt data.
+        Added in the python version
+
+        :param encrypted_data: Bytes to decrypt
+        :return: Decrypted bytes
+        """
+        self.reset()
+        decrypted_data = []
+        for b in bytearray(encrypted_data):
+            decrypted_data.append(self.decrypt(b))
+        # Doing both bytes and bytearray to make it work on both python2 and 3
+        return bytes(bytearray(decrypted_data))
+
+    def decrypt(self, b):
+        """
+        Decrypts a byte
+
+        :param b: Byte to decrypt
+        :return: Decrypted byte
+        """
+        swaptemp = 0
+
+        # Shuffle the deck a little more.
+        self.ratchet = (self.ratchet + self.cards[self.rotor]) & 0xFF
+        self.rotor = (1 + self.rotor) & 0xFF
+        swaptemp = self.cards[self.last_cipher]
+        self.cards[self.last_cipher] = self.cards[self.ratchet]
+        self.cards[self.ratchet] = self.cards[self.last_plain]
+        self.cards[self.last_plain] = self.cards[self.rotor]
+        self.cards[self.rotor] = swaptemp
+        self.avalanche = (self.avalanche + self.cards[swaptemp]) & 0xFF
+
+        # Output one byte from the state in such a way as to make it
+        # very hard to figure out which one you are looking at.
+        self.last_plain = b ^ self.cards[(self.cards[self.ratchet] + self.cards[self.rotor]) & 0xFF] ^ \
+            self.cards[self.cards[(self.cards[self.last_plain] + self.cards[self.last_cipher] +
+                                   self.cards[self.avalanche]) & 0xFF]]
+        self.last_cipher = b
+        return self.last_plain
+
+    def hash_final(self, hashlength):
+        """
+        Generates a hash of the given length
+
+        :param hashlength: Length of the hash to generate
+        :return: Decrypted byte
+        """
+        for i in range(255, -1, -1):
+            self.encrypt(i)
+        hash_out = []
+        for i in range(0, hashlength):
+            hash_out.append(self.encrypt(0))
+        # Doing both bytes and bytearray to make it work on both python2 and 3
+        return bytes(bytearray(hash_out))
