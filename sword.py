bd67e7a5510e880ca4ea048fc44c8a1be4283226
Dan Safee <dan@getdrx.com>
2024-10-20 21:34:54 -0400

    Update bible data from scraper

--- sword.py
+++ sword.py
@@ -1,9 +1,9 @@
 from pysword.bible import SwordBible
-# Create the bible. The arguments are:
-# SwordBible(<module path>, <module type>, <versification>, <encoding>, <text formatting>)
-# Only the first is required, the rest have default values which should work in most cases.
+from youscraper import get_chapter
+import os
+
 bible = SwordBible('/home/dansafee/Desktop/sword/modules/texts/ztext/kjvpce/')
-# Get John chapter 3 verse 16
+
 output = bible.get(books=['john', 'matthew'], chapters=[3], verses=[16])
 
 
@@ -17,12 +17,22 @@ nt_books = [book.name for book in books['nt']]
 books = ot_books + nt_books
 
 print(books)
+books = ['HEB']
 
 for book in books: 
     for chapter in range(1, 800):
-        for verse in range(1, 800):
-            try:
-                output = bible.get(books=[book], chapters=[chapter], verses=[verse])
-                print(f'{book} {chapter}:{verse} - {output}')
-            except:
-                break
+        chapter_data = get_chapter(book[0:3].upper(), chapter)
+
+        try:
+            os.mkdir(f'bible/{book}')
+        except:
+            pass
+
+        with open(f'bible/{book}/{str(chapter).zfill(3)}.txt', 'w') as f:
+            f.write(chapter_data['pageProps']['chapterInfo']['content'])
+        # for verse in range(1, 800):
+        #     try:
+        #         output = bible.get(books=[book], chapters=[chapter], verses=[verse])
+        #         print(f'{book} {chapter}:{verse} - {output}')
+        #     except:
+        #         break
