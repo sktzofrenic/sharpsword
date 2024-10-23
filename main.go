0007866eabb70ab4f5d53e1ff6a5cfb9cc68fab1
Dan Safee <dan@getdrx.com>
2024-10-23 06:32:52 -0400

    Add Bible API

--- main.go
+++ main.go
@@ -3,10 +3,16 @@ package main
 import (
     "log"
     "github.com/gofiber/fiber/v3"
+    "github.com/gofiber/fiber/v3/middleware/static"
+    "sharpsword/sharpsword/api/bible"
 )
 
 func main() {
     app := fiber.New()
+
+    app.Get("/public/*", static.New(("./public")))
+
+    bible.Register(app)
 
     app.Get("/", func(c fiber.Ctx) error {
         return c.SendString("Hello, World 👋!")
