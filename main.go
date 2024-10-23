153e60f5c52a2ea888b8f1471dd776d47332cb29
Dan Safee <dan@getdrx.com>
2024-10-22 21:04:48 -0400

    Add go backend for sharp sword

---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+    "log"
+    "github.com/gofiber/fiber/v3"
+)
+
+func main() {
+    app := fiber.New()
+
+    app.Get("/", func(c fiber.Ctx) error {
+        return c.SendString("Hello, World 👋!")
+    })
+
+    log.Fatal(app.Listen(":3000"))
+}
