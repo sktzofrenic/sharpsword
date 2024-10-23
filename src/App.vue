fbd28f44e5f32830b139ede0c8f727c0177b2411
Dan Safee <dan@getdrx.com>
2024-10-23 14:14:39 -0400

    Update to latest rendering

--- src/App.vue
+++ src/App.vue
@@ -1,102 +1,92 @@
 
 <template>
     <div class="min-h-full">
-        <nav class="bg-slate-950">
-            <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+        <nav class="bg-slate-950/30 sticky top-0 backdrop-blur-sm">
+            <div class="mx-auto max-w-7xl pr-4">
                 <div class="flex h-16 items-center justify-between">
                     <div class="flex items-center">
                         <div class="flex-shrink-0">
-                            <img class="h-8 w-8" src="@/assets/sword_zoom_sq_512.png" alt="Your Company">
-                        </div>
-                        <div class="hidden md:block">
-                            <div class="ml-10 flex items-baseline space-x-4">
-                                <!-- Current: "bg-slate-900 text-white", Default: "text-slate-300 hover:bg-slate-700 hover:text-white" -->
-                                <a href="#" class="rounded-md bg-slate-900 px-3 py-2 text-sm font-medium text-white" aria-current="page">Bible</a>
-                            </div>
+                            <img class="h-16 w-16 border-r border-b border-slate-700 rounded-md" src="@/assets/sword_zoom_sq_512.png" alt="Your Company">
                         </div>
                     </div>
-                    <div class="hidden md:block">
-                        <div class="ml-4 flex items-center md:ml-6">
-                            <!-- Profile dropdown -->
-                            <div class="relative ml-3">
-                                <div>
-                                    <button type="button" class="relative flex max-w-xs items-center rounded-full bg-slate-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-slate-800" id="user-menu-button" aria-expanded="false" aria-haspopup="true">
-                                        <span class="absolute -inset-1.5"></span>
-                                        <span class="sr-only">Open user menu</span>
-                                        <i class="fa-solid fa-user text-white text-xl"></i>
-                                    </button>
-                                </div>
-                                <!--
-Dropdown menu, show/hide based on menu state.
-
-Entering: "transition ease-out duration-100"
-From: "transform opacity-0 scale-95"
-To: "transform opacity-100 scale-100"
-Leaving: "transition ease-in duration-75"
-From: "transform opacity-100 scale-100"
-To: "transform opacity-0 scale-95"
--->
-                                <div class="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none" 
-                                    v-if="false"
-                                    role="menu" aria-orientation="vertical" aria-labelledby="user-menu-button" tabindex="-1">
-                                    <!-- Active: "bg-slate-100", Not Active: "" -->
-                                    <a href="#" class="block px-4 py-2 text-sm text-slate-700" role="menuitem" tabindex="-1" id="user-menu-item-0">Your Profile</a>
-                                    <a href="#" class="block px-4 py-2 text-sm text-slate-700" role="menuitem" tabindex="-1" id="user-menu-item-1">Settings</a>
-                                    <a href="#" class="block px-4 py-2 text-sm text-slate-700" role="menuitem" tabindex="-1" id="user-menu-item-2">Sign out</a>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="-mr-2 flex md:hidden">
+                    <div class="-mr-2 flex">
                         <!-- Mobile menu button -->
-                        <button type="button" class="relative inline-flex items-center justify-center rounded-md bg-slate-800 p-2 text-slate-400 hover:bg-slate-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-slate-800" aria-controls="mobile-menu" aria-expanded="false">
+                        <button type="button" class="relative inline-flex items-center justify-center rounded-md bg-slate-800 py-1 px-2 text-slate-400 hover:bg-slate-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-slate-800 mr-2" aria-controls="mobile-menu" aria-expanded="false">
                             <span class="absolute -inset-0.5"></span>
-                            <span class="sr-only">Open main menu</span>
-                            <!-- Menu open: "hidden", Menu closed: "block" -->
-                            <svg class="block h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true" data-slot="icon">
-                                <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
-                            </svg>
-                            <!-- Menu open: "block", Menu closed: "hidden" -->
-                            <svg class="hidden h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true" data-slot="icon">
-                                <path stroke-linecap="round" stroke-linejoin="round" d="M6 18 18 6M6 6l12 12" />
-                            </svg>
+                            <i class="fa-solid fa-text-size"></i>
+                        </button>
+                        <button type="button" class="relative inline-flex items-center justify-center rounded-md bg-slate-800 py-1 px-2 text-slate-400 hover:bg-slate-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-slate-800 mr-2" aria-controls="mobile-menu" aria-expanded="false">
+                            <span class="absolute -inset-0.5"></span>
+                            <i class="fa-solid fa-magnifying-glass"></i>
+                        </button>
+                        <button type="button" class="relative inline-flex items-center justify-center rounded-md bg-slate-800 py-1 px-2 text-slate-400 hover:bg-slate-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-slate-800" aria-controls="mobile-menu" aria-expanded="false">
+                            <span class="absolute -inset-0.5"></span>
+                            <i class="fa-sharp-duotone fa-solid fa-book-bible mr-1"></i> KJV
                         </button>
                     </div>
                 </div>
             </div>
-
-            <!-- Mobile menu, show/hide based on menu state. -->
-            <div class="md:hidden" id="mobile-menu">
-                <div class="space-y-1 px-2 pb-3 pt-2 sm:px-3">
-                    <!-- Current: "bg-slate-900 text-white", Default: "text-slate-300 hover:bg-slate-700 hover:text-white" -->
-                    <a href="#" class="block rounded-md bg-slate-900 px-3 py-2 text-base font-medium text-white" aria-current="page">Bible</a>
+        </nav>
+        <main>
+            <div class="mx-auto max-w-7xl px-6 py-6 sm:px-6 lg:px-8 text-slate-100 pb-40">
+                <div class="w-full flex flex-col justify-center items-center">
+                    <div class="text-md font-semi-bold">Psalm</div>
+                    <div class="font-bold text-6xl">30</div>
                 </div>
-                <div class="border-t border-slate-700 pb-3 pt-4">
-                    <div class="mt-3 space-y-1 px-2">
-                        <a href="#" class="block rounded-md px-3 py-2 text-base font-medium text-slate-400 hover:bg-slate-700 hover:text-white">Your Profile</a>
-                        <a href="#" class="block rounded-md px-3 py-2 text-base font-medium text-slate-400 hover:bg-slate-700 hover:text-white">Settings</a>
-                        <a href="#" class="block rounded-md px-3 py-2 text-base font-medium text-slate-400 hover:bg-slate-700 hover:text-white">Sign out</a>
+                <div class="w-full italic px-4 my-6 flex justify-center text-lg">
+                    A Psalm and Song at the dedication of the house of David.
+                </div>
+                <div class="flex justify-center leading-loose text-lg">
+                    <div class="chapter ch30" data-usfm="PSA.30">
+                        <div class="q1"><span class="content">  </span><span class="verse v1" data-usfm="PSA.30.1"><span class="label">1</span><span class="content">I will extol thee, O </span><span class="nd"><span class="content">LORD</span></span><span class="content">; for thou hast lifted me up,</span></span></div><div class="q2"><span class="verse v1" data-usfm="PSA.30.1"><span class="content">And hast not made my foes to rejoice over me.</span></span></div><div class="q1"><span class="verse v1" data-usfm="PSA.30.1"><span class="content">  </span></span><span class="verse v2" data-usfm="PSA.30.2"><span class="label">2</span><span class="content">O </span><span class="nd"><span class="content">LORD</span></span><span class="content"> my God,</span></span></div><div class="q2"><span class="verse v2" data-usfm="PSA.30.2"><span class="content">I cried unto thee, and thou hast healed me.</span></span></div><div class="q1"><span class="verse v2" data-usfm="PSA.30.2"><span class="content">  </span></span><span class="verse v3" data-usfm="PSA.30.3"><span class="label">3</span><span class="content">O </span><span class="nd"><span class="content">LORD</span></span><span class="content">, thou hast brought up my soul from the grave:</span></span></div><div class="q2"><span class="verse v3" data-usfm="PSA.30.3"><span class="content">Thou hast kept me alive, that I should not go down to the pit.</span></span></div><div class="b"></div><div class="q1"><span class="verse v3" data-usfm="PSA.30.3"><span class="content">  </span></span><span class="verse v4" data-usfm="PSA.30.4"><span class="label">4</span><span class="content">Sing unto the </span><span class="nd"><span class="content">LORD</span></span><span class="content">, O ye saints of his,</span></span></div><div class="q2"><span class="verse v4" data-usfm="PSA.30.4"><span class="content">And give thanks at the remembrance of his holiness.</span></span></div><div class="q1"><span class="verse v4" data-usfm="PSA.30.4"><span class="content">  </span></span><span class="verse v5" data-usfm="PSA.30.5"><span class="label">5</span><span class="content">For his anger </span><span class="add"><span class="content">endureth but</span></span><span class="content"> a moment; in his favour </span><span class="add"><span class="content">is</span></span><span class="content"> life:</span></span></div><div class="q2"><span class="verse v5" data-usfm="PSA.30.5"><span class="content">Weeping may endure for a night, but joy </span><span class="add"><span class="content">cometh</span></span><span class="content"> in the morning.</span></span></div><div class="b"></div><div class="q1"><span class="verse v5" data-usfm="PSA.30.5"><span class="content">  </span></span><span class="verse v6" data-usfm="PSA.30.6"><span class="label">6</span><span class="content">And in my prosperity I said, I shall never be moved.</span></span></div><div class="q2"><span class="verse v6" data-usfm="PSA.30.6"><span class="content">  </span></span><span class="verse v7" data-usfm="PSA.30.7"><span class="label">7</span><span class="content">  </span><span class="nd"><span class="content">LORD</span></span><span class="content">, by thy favour thou hast made my mountain to stand strong:</span></span></div><div class="q1"><span class="verse v7" data-usfm="PSA.30.7"><span class="content">Thou didst hide thy face, </span><span class="add"><span class="content">and</span></span><span class="content"> I was troubled.</span></span></div><div class="b"></div><div class="q1"><span class="verse v7" data-usfm="PSA.30.7"><span class="content">  </span></span><span class="verse v8" data-usfm="PSA.30.8"><span class="label">8</span><span class="content">I cried to thee, O </span><span class="nd"><span class="content">LORD</span></span><span class="content">;</span></span></div><div class="q2"><span class="verse v8" data-usfm="PSA.30.8"><span class="content">And unto the </span><span class="nd"><span class="content">LORD</span></span><span class="content"> I made supplication.</span></span></div><div class="q1"><span class="verse v8" data-usfm="PSA.30.8"><span class="content">  </span></span><span class="verse v9" data-usfm="PSA.30.9"><span class="label">9</span><span class="content">What profit </span><span class="add"><span class="content">is there</span></span><span class="content"> in my blood,</span></span></div><div class="q2"><span class="verse v9" data-usfm="PSA.30.9"><span class="content">When I go down to the pit?</span></span></div><div class="q1"><span class="verse v9" data-usfm="PSA.30.9"><span class="content">Shall the dust praise thee?</span></span></div><div class="q2"><span class="verse v9" data-usfm="PSA.30.9"><span class="content">Shall it declare thy truth?</span></span></div><div class="q1"><span class="verse v9" data-usfm="PSA.30.9"><span class="content">  </span></span><span class="verse v10" data-usfm="PSA.30.10"><span class="label">10</span><span class="content">Hear, O </span><span class="nd"><span class="content">LORD</span></span><span class="content">, and have mercy upon me:</span></span></div><div class="q2"><span class="verse v10" data-usfm="PSA.30.10"><span class="content">  </span><span class="nd"><span class="content">LORD</span></span><span class="content">, be thou my helper.</span></span></div><div class="b"></div><div class="q1"><span class="verse v10" data-usfm="PSA.30.10"><span class="content">  </span></span><span class="verse v11" data-usfm="PSA.30.11"><span class="label">11</span><span class="content">Thou hast turned for me my mourning into dancing:</span></span></div><div class="q2"><span class="verse v11" data-usfm="PSA.30.11"><span class="content">Thou hast put off my sackcloth, and girded me with gladness;</span></span></div><div class="q1"><span class="verse v11" data-usfm="PSA.30.11"><span class="content">  </span></span><span class="verse v12" data-usfm="PSA.30.12"><span class="label">12</span><span class="content">To the end that </span><span class="add"><span class="content">my</span></span><span class="content"> glory may sing praise to thee, and not be silent.</span></span></div><div class="q2"><span class="verse v12" data-usfm="PSA.30.12"><span class="content">O </span><span class="nd"><span class="content">LORD</span></span><span class="content"> my God, I will give thanks unto thee for ever.</span></span></div><div class="b"></div>
+                    </div>
+
+                </div>
+
+            </div>
+        </main>
+        <footer class="bg-slate-950 fixed w-full bottom-0 border-slate-600 border-t">
+            <div class="mx-auto max-w-7xl px-4 text-slate-200 flex items-center justify-center">
+                <div class="flex justify-between w-full text-center bg-slate-900 my-2 rounded-2xl py-2">
+                    <i class="fa-solid fa-left text-xl px-4 text-slate-500"></i>
+                    <span class="font-bold py-1">Exodus 45</span>
+                    <i class="fa-solid fa-right text-xl px-4 text-slate-500"></i>
+                </div>
+            </div>
+            <div class="mx-auto max-w-7xl px-8 pb-2">
+                <div class="flex h-16 items-center justify-between">
+                    <div class="flex flex-col items-center text-slate-200 w-14 bg-slate-900 rounded-xl px-8 py-2">
+                        <i class="fa-sharp-duotone fa-solid fa-book-bible text-xl block"></i>
+                        <span class="text-xs">Bible</span>
+                    </div>
+                    <div class="flex flex-col items-center text-slate-200 w-14">
+                        <i class="fa-sharp-duotone fa-solid fa-ballot-check text-xl block"></i>
+                        <span class="text-xs">Plans</span>
+                    </div>
+                    <div class="flex flex-col items-center text-slate-200 w-14">
+                        <i class="fa-solid fa-magnifying-glass text-xl block"></i>
+                        <span class="text-xs">Search</span>
+                    </div>
+                    <div class="flex flex-col items-center text-slate-200 w-14">
+                        <i class="fa-sharp-duotone fa-solid fa-highlighter-line text-xl block"></i>
+                        <span class="text-xs">Highlights</span>
                     </div>
                 </div>
             </div>
-        </nav>
-
-        <header class="bg-slate-900 shadow">
-            <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-                <h1 class="text-3xl font-bold tracking-tight text-gray-100">Genesis 1:1</h1>
-            </div>
-        </header>
-        <main>
-            <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-                <!-- Your content -->
-            </div>
-        </main>
+        </footer>
     </div>
 </template>
 
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
 </script>
 <style scoped>
+.label {
+    vertical-align: top;
+    font-size: 0.8em;
+    margin-right: 0.5em;
+    margin-left: 1.2em;
+    color: #9ca3af;
+}
+
 </style>
