8ba82ed99e7da9f9db1fb7387edba10fc28c3ad1
Dan Safee <dan@getdrx.com>
2024-10-24 16:13:00 -0400

    Add databse

--- go/api/bible/bible.go
+++ go/api/bible/bible.go
@@ -6,6 +6,10 @@ import (
 
 func Register(app *fiber.App) {
     app.Get("/api/v1/bible", func(c fiber.Ctx) error {
+        // Connect with database
+        if err := Connect(); err != nil {
+            log.Fatal(err)
+        }
         return c.JSON(fiber.Map{
             "message": "Hello, World 📖!",
         })
