54e97cfa3095a3ff04ea1feb1dc53c12ba42b342
Dan Safee <dan@getdrx.com>
2024-10-24 17:25:57 -0400

    Successfully query the database

--- go/api/bible/bible.go
+++ go/api/bible/bible.go
@@ -2,16 +2,52 @@ package bible
 
 import (
     "github.com/gofiber/fiber/v3"
+    "github.com/jackc/pgx/v5"
+    "fmt"
+    "os"
+    "context"
 )
 
-func Register(app *fiber.App) {
+func Register(app *fiber.App, databaseURL string) {
     app.Get("/api/v1/bible", func(c fiber.Ctx) error {
-        // Connect with database
-        if err := Connect(); err != nil {
-            log.Fatal(err)
+
+        conn, err := pgx.Connect(context.Background(), databaseURL)
+
+        if err != nil {
+            fmt.Fprintf(os.Stderr, "Unable to connect to database: %v\n", err)
+            os.Exit(1)
         }
+        defer conn.Close(context.Background())
+
+        rows, err := conn.Query(context.Background(), "SELECT * FROM verses LIMIT 1")
+
+        var rowSlice []string
+        for rows.Next() {
+            type rowType struct {
+                verse_id int
+                book int
+                chapter int
+                verse int
+                text_plain string
+                text_formatted string
+                version string
+            }
+            var row rowType
+            err := rows.Scan(&row.verse_id, &row.book, &row.chapter, &row.verse, &row.text_plain, &row.text_formatted, &row.version)
+            if err != nil {
+                fmt.Fprintf(os.Stderr, "Scan failed: %v\n", err)
+            }
+            rowSlice = append(rowSlice, row.text_plain)
+        }
+        
+        if err != nil {
+            fmt.Fprintf(os.Stderr, "QueryRow failed: %v\n", err)
+            os.Exit(1)
+        }
+
         return c.JSON(fiber.Map{
-            "message": "Hello, World 📖!",
+            "status": "success",
+            "verses": rowSlice,
         })
     })
 }
