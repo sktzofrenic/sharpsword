f7a1fda14c91bbdc0fe143929b252930e399b62d
Dan Safee <dan@getdrx.com>
2024-10-25 06:04:02 -0400

    Update table structure

--- data/table_structure.sql
+++ data/table_structure.sql
@@ -1,4 +1,4 @@
-create table if not exists public.books
+create table books
 (
     book_id           integer not null
         constraint books_pk
@@ -8,45 +8,51 @@ create table if not exists public.books
     testament         text
 );
 
-create table if not exists public.verses
+create table verses
 (
     verse_id       integer not null,
     book           integer
         constraint verses_books_book_id_fk
-            references public.books,
+            references books,
     chapter        integer,
     verse          integer,
     text_plain     text,
     text_formatted text,
     version        text    not null,
+    ts_vec         tsvector,
     constraint verses_pk
         primary key (verse_id, version)
 );
 
-create table if not exists public.paragraphs
+create index ix_verses_tsv
+    on verses using gin (ts_vec);
+
+create index verses_verse_id_index
+    on verses (verse_id);
+
+create table paragraphs
 (
     verse_id integer,
     version  text,
     constraint paragraphs_verses_verse_id_version_fk
-        foreign key (verse_id, version) references public.verses
+        foreign key (verse_id, version) references verses
 );
 
-create table if not exists public.descriptions
+create table descriptions
 (
     verse_id          integer,
     version           text,
     description_plain text,
     constraint descriptions_verses_verse_id_version_fk
-        foreign key (verse_id, version) references public.verses
+        foreign key (verse_id, version) references verses
 );
 
-create table if not exists public.headings
+create table headings
 (
     verse_id      integer,
     version       text,
     heading_plain text,
     constraint headings_verses_verse_id_version_fk
-        foreign key (verse_id, version) references public.verses
+        foreign key (verse_id, version) references verses
 );
-
 
