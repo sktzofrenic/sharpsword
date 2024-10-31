fb6a98b8e5b21b2956e609497d5b6d616d37be1f
Dan Safee <dan@getdrx.com>
2024-10-31 06:57:26 -0400

    Add custom search dictionary with no stop wrods

--- data/table_structure.sql
+++ data/table_structure.sql
@@ -73,3 +73,11 @@ create table public.headings
         foreign key (verse_id, version) references public.verses
 );
 
+CREATE TEXT SEARCH DICTIONARY english_stem_nostop (
+    Template = snowball
+    , Language = english
+    );
+
+CREATE TEXT SEARCH CONFIGURATION public.english_nostop ( COPY = pg_catalog.english );
+ALTER TEXT SEARCH CONFIGURATION public.english_nostop
+    ALTER MAPPING FOR asciiword, asciihword, hword_asciipart, hword, hword_part, word WITH english_stem_nostop;
