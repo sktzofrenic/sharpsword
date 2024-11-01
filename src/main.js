bc7720568b5d05a38391eb2d7eecfb418b9efa3c
Dan Safee <dan@getdrx.com>
2024-11-01 06:02:46 -0400

    Remove router from project

--- src/main.js
+++ src/main.js
@@ -4,13 +4,11 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
 import App from './App.vue'
-import router from './router'
 
 const worker = new Worker(new URL('./assets/js/service-worker.js', import.meta.url))
 
 const app = createApp(App)
 
 app.use(createPinia())
-app.use(router)
 
 app.mount('#app')
